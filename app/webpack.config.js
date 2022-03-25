f7d3f311f59b3ee7dba79c17bb088ef81b57df32
Paulo Faulstich <paulo.faulstich@gmail.com>
2022-03-24 23:35:20 -0700

    running project

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -8,7 +8,13 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [
-    new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "./src/index.html", to: "index.html" }        
+      ]
+    }),
   ],
-  devServer: { contentBase: path.join(__dirname, "dist"), compress: true },
+  devServer: { static: path.join(__dirname, "dist"), compress: true },
 };
+// contentBase: path.join(__dirname, "app"),
+// new CopyWebpackPlugin([{ from: "./src/index.html", to: "index.html" }]),
